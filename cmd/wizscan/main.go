2b11d5d27e9a85a94c6f5b279a39c86df9385d38
jtb75 <joe.buhr@gmail.com>
2024-04-10 07:52:14 -0500

    Updated test harness

--- cmd/wizscan/main.go
+++ cmd/wizscan/main.go
@@ -69,129 +69,101 @@ func main() {
 
 	var assetVulns vulnerability.Asset
 
-	// Set test to 0 to run, set to 1 to use sample data
-	test := 0
-	if test == 0 {
+	jsonResponseBytes, err := json.MarshalIndent(response, "", "    ")
+	if err != nil {
+		fmt.Println("Error marshalling JSON:", err)
+		return
+	}
+	// Writing the indented JSON output to a file
+	err = os.WriteFile("sample_data/known_vulns.json", jsonResponseBytes, 0644)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		logger.Log.Exit(1)
+		return
+	}
 
-		jsonResponseBytes, err := json.MarshalIndent(response, "", "    ")
-		if err != nil {
-			fmt.Println("Error marshalling JSON:", err)
-			return
-		}
-		// Writing the indented JSON output to a file
-		err = os.WriteFile("sample_data/known_vulns.json", jsonResponseBytes, 0644)
-		if err != nil {
-			fmt.Println("Error writing to file:", err)
-			logger.Log.Exit(1)
-			return
-		}
+	// Initialize and authenticate wizcli
+	cleanup, wizCliPath, err := wizcli.InitializeAndAuthenticate(args.WizClientID, args.WizClientSecret)
+	if err != nil {
+		logger.Log.Errorf("Initialization and authentication failed: %v", err)
+		return
+	}
+	defer cleanup()
 
-		// Initialize and authenticate wizcli
-		cleanup, wizCliPath, err := wizcli.InitializeAndAuthenticate(args.WizClientID, args.WizClientSecret)
-		if err != nil {
-			logger.Log.Errorf("Initialization and authentication failed: %v", err)
-			return
-		}
-		defer cleanup()
+	// Retrieve top-level directories
+	directories, err := utility.GetTopLevelDirectories()
+	if err != nil {
+		logger.Log.Errorf("Error listing directories: %v", err)
+		return
+	} else {
+		logger.Log.Debug("Directories to scan: ", directories)
+	}
 
-		// Retrieve top-level directories
-		directories, err := utility.GetTopLevelDirectories()
-		if err != nil {
-			logger.Log.Errorf("Error listing directories: %v", err)
-			return
-		} else {
-			logger.Log.Debug("Directories to scan: ", directories)
-		}
+	aggregatedResults := wizcli.AggregatedScanResults{}
 
-		aggregatedResults := wizcli.AggregatedScanResults{}
+	// Used for testing
+	//directories = []string{"/boot", "/usr"}
+	//directories = []string{"E:\\"}
 
-		// Used for testing
-		//directories = []string{"/boot", "/usr"}
-		//directories = []string{"E:\\"}
-
-		logger.Log.Info("Initiating directory scan")
-		for _, drive := range directories {
-			mountedPath := ""
-			shadowCopyID := ""
-			if runtime.GOOS == "windows" {
-				mountedPath, shadowCopyID, err = utility.CreateVSSSnapshot(drive)
-				if err != nil {
-					logger.Log.Errorf("Error creating VSS snapshot for drive %s: %v", drive, err)
-					continue
-				}
-			}
-
-			// Do operations on the mounted snapshot...
-			if mountedPath == "" {
-				mountedPath = drive
-			}
-			scanResult, err := wizcli.ScanDirectory(wizCliPath, mountedPath)
+	logger.Log.Info("Initiating directory scan")
+	for _, drive := range directories {
+		mountedPath := ""
+		shadowCopyID := ""
+		if runtime.GOOS == "windows" {
+			mountedPath, shadowCopyID, err = utility.CreateVSSSnapshot(drive)
 			if err != nil {
-				logger.Log.Errorf("Failed to scan %s: %v", mountedPath, err)
+				logger.Log.Errorf("Error creating VSS snapshot for drive %s: %v", drive, err)
 				continue
 			}
+		}
 
-			// Prepend the Drive to the Library path to represent actual full path
-			for i, lib := range scanResult.Result.Libraries {
-				if runtime.GOOS == "windows" {
-					lib.Path = strings.ReplaceAll(lib.Path, "/", "\\")
-					lib.Path = strings.TrimPrefix(lib.Path, "\\")
-				}
-				scanResult.Result.Libraries[i].Path = drive + lib.Path
-			}
+		// Do operations on the mounted snapshot...
+		if mountedPath == "" {
+			mountedPath = drive
+		}
+		scanResult, err := wizcli.ScanDirectory(wizCliPath, mountedPath)
+		if err != nil {
+			logger.Log.Errorf("Failed to scan %s: %v", mountedPath, err)
+			continue
+		}
 
-			// Aggregate results
-			aggregatedResults.Libraries = append(aggregatedResults.Libraries, scanResult.Result.Libraries...)
-			aggregatedResults.Applications = append(aggregatedResults.Applications, scanResult.Result.Applications...)
-			// Remove the VSS snapshot and link
+		// Prepend the Drive to the Library path to represent actual full path
+		for i, lib := range scanResult.Result.Libraries {
 			if runtime.GOOS == "windows" {
-
-				if err := utility.RemoveVSSSnapshot(mountedPath, shadowCopyID); err != nil {
-					logger.Log.Errorf("Failed to remove mount and VSS snapshot for drive %s: %v", drive, err)
-				}
+				lib.Path = strings.ReplaceAll(lib.Path, "/", "\\")
+				lib.Path = strings.TrimPrefix(lib.Path, "\\")
 			}
-
-		}
-		jsonBytes, err := json.MarshalIndent(aggregatedResults, "", "    ")
-		if err != nil {
-			fmt.Println("Error marshalling JSON:", err)
-			return
+			scanResult.Result.Libraries[i].Path = drive + lib.Path
 		}
 
-		err = os.WriteFile("sample_data/scan.json", jsonBytes, 0644)
-		if err != nil {
-			fmt.Println("Error writing to file:", err)
-			return
+		// Aggregate results
+		aggregatedResults.Libraries = append(aggregatedResults.Libraries, scanResult.Result.Libraries...)
+		aggregatedResults.Applications = append(aggregatedResults.Applications, scanResult.Result.Applications...)
+		// Remove the VSS snapshot and link
+		if runtime.GOOS == "windows" {
+
+			if err := utility.RemoveVSSSnapshot(mountedPath, shadowCopyID); err != nil {
+				logger.Log.Errorf("Failed to remove mount and VSS snapshot for drive %s: %v", drive, err)
+			}
 		}
 
-		assetVulns, err = vulnerability.CompareVulnerabilities(aggregatedResults, response, args.ScanProviderID)
-		if err != nil {
-			fmt.Printf("Error in CompareVulnerabilities: %s\n", err)
-			return
-		}
+	}
+	jsonBytes, err := json.MarshalIndent(aggregatedResults, "", "    ")
+	if err != nil {
+		fmt.Println("Error marshalling JSON:", err)
+		return
+	}
 
-	} else {
+	err = os.WriteFile("sample_data/scan.json", jsonBytes, 0644)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
 
-		// Load the scan results into aggregatedResults using LoadScanResults
-		// which returns a pointer to AggregatedScanResults
-		filePath := "sample_data/scan.json" // Adjust the file path as necessary
-		loadedResults, err := wizcli.LoadScanResults(filePath)
-		if err != nil {
-			fmt.Printf("Error loading scan results: %s\n", err)
-			return
-		}
-
-		// Ensure loadedResults is not nil before dereferencing
-		if loadedResults == nil {
-			fmt.Println("No scan results loaded, loadedResults is nil")
-			return
-		}
-		assetVulns, err = vulnerability.CompareVulnerabilities(*loadedResults, response, args.ScanProviderID)
-		if err != nil {
-			fmt.Printf("Error in CompareVulnerabilities: %s\n", err)
-			return
-		}
-
+	assetVulns, err = vulnerability.CompareVulnerabilities(aggregatedResults, response, args.ScanProviderID)
+	if err != nil {
+		fmt.Printf("Error in CompareVulnerabilities: %s\n", err)
+		return
 	}
 
 	var vulnPayloadJSON []byte // Use a byte slice to hold JSON data
